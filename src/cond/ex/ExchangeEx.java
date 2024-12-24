c4fb1efb9bc59ea401e7043541ef205d3bddc8b8
oms <journy002@naver.com>
2024-12-24 10:38:44 +0900

    조건문 예제 연습

---
+++ src/cond/ex/ExchangeEx.java
@@ -0,0 +1,18 @@
+package cond.ex;
+
+public class ExchangeEx {
+    public static void main(String[] args) {
+        int dollar = 3;
+
+        if(dollar > 0) {
+            int won = dollar * 1300;
+            System.out.println("환전 금액은: " + won);
+        } else if (dollar == 0) {
+            System.out.println("환전할 금액이 없습니다.");
+        } else {
+            System.out.println("잘못된 금액입니다.");
+        }
+
+
+    }
+}
