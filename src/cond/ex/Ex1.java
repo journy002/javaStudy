779264a20f154a40a28ce5766334e88c70910a2e
oms <journy002@naver.com>
2024-12-24 09:45:51 +0900

    Study swtich

--- src/cond/ex/Ex1.java
+++ src/cond/ex/Ex1.java
@@ -10,6 +10,7 @@ public class Ex1 {
             discount += 1000;
             System.out.println("10000원 이상 구매, 1000원 할인");
         }
+        // else if문을 사용하면 첫 번째 조건을 만족하는 순간 조건문을 나오게 되어 두 번째 할인 혜택을 받지 못하는 상황이 발생한다.
 
         if (age <= 10) {
             discount += 1000;
