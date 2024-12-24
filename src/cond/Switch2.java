4f870099fdd328cca39d43d68ef3f24c26bc9b3b
oms <journy002@naver.com>
2024-12-24 09:49:05 +0900

    Study swtich2

--- src/cond/Switch2.java
+++ src/cond/Switch2.java
@@ -1,11 +1,10 @@
 package cond;
 
-public class Switch1 {
+public class Switch2 {
     public static void main(String[] args) {
-        //grade 1:1000, 2:2000, 3:3000, 나머지: 500
+        //grade 1:1000, 2,3:3000, 나머지: 500
 
         int grade = 2;
-
         int coupon;
 
         switch (grade) {
@@ -13,8 +12,6 @@ public class Switch1 {
                 coupon = 1000;
                 break;
             case 2:
-                coupon = 2000;
-                break;
             case 3:
                 coupon = 3000;
                 break;
